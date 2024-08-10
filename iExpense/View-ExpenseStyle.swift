e00e2e0af644cc0768520abd510ee0867182a2aa
Adam Sayer <asayer82@icloud.com>
2024-08-10 22:02:34 +1000

    Added in changes so ready for the migrate to SwiftData Project

---
+++ iExpense/View-ExpenseStyle.swift
@@ -0,0 +1,18 @@
+//
+//  View-ExpenseStyle.swift
+//  iExpense
+//
+//  Created by Adam Sayer on 10/8/2024.
+//
+
+import SwiftUI
+
+struct View_ExpenseStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View_ExpenseStyle()
+}
